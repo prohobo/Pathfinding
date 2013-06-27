7eee85f596caa25afe44a587908f3013a17d4e24
Alex Klos <aklos34@gmail.fake>
2013-06-27 18:24:31 +0200

    setting up project

---
+++ PathfindingApp/PathfindingApp/AStar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindingApp
+{
+    class AStar : Algorithm
+    {
+        public AStar()
+        {
+
+        }
+    }
+}
